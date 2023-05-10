54f6fdea75d9fa01595bb4530e746bd877a6f8fa
Samirbek <tolibovsamirbek070@gmail.com>
2023-05-10 15:41:30 +0500

    models.py, admin.py, urls.py, views.py

--- config/models.py
+++ config/models.py
@@ -11,6 +11,9 @@ class Login(models.Model):
     password = models.IntegerField(max_length=150, null=True, blank=True)
     confirm_password = models.IntegerField(max_length=150, null=True, blank=True)
     
+    def __str__(self) -> str:
+        return self.username
+    
 class Food(models.Model):
     food_name = models.CharField(max_length=250, null=True, blank=True)
     food_price = models.IntegerField(max_length=200, null=True, blank=True)
@@ -18,8 +21,14 @@ class Food(models.Model):
     food_image = models.ImageField(upload_to="fast-food", null=True, blank=True)
     prep_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     
+    def __str__(self) -> str:
+        return self.food_name
+    
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     order_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     table = models.CharField(max_length=120, null=True, blank=True)
     food = models.ManyToManyField(Food, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.user
--- config/views.py
+++ config/views.py
@@ -1,3 +1,236 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import *
 
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+    
+    
+    
+  
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        pic = request.FILES.get('picture')
+        try:
+            post = Food.objects.create(creater=request.user, content_text=text, content_image=pic)
+            return HttpResponseRedirect(reverse('index'))
+        except Exception as e:
+            return HttpResponse(e)
+    else:
+        return HttpResponse("Method must be 'POST'")
+
+@login_required
+@csrf_exempt
+def edit_post(request, post_id):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        pic = request.FILES.get('picture')
+        img_chg = request.POST.get('img_change')
+        post_id = request.POST.get('id')
+        post = Food.objects.get(id=post_id)
+        try:
+            post.content_text = text
+            if img_chg != 'false':
+                post.content_image = pic
+            post.save()
+            
+            if(post.content_text):
+                post_text = post.content_text
+            else:
+                post_text = False
+            if(post.content_image):
+                post_image = post.img_url()
+            else:
+                post_image = False
+            
+            return JsonResponse({
+                "success": True,
+                "text": post_text,
+                "picture": post_image
+            })
+        except Exception as e:
+            print('-----------------------------------------------')
+            print(e)
+            print('-------------------------------------------  ----')
+            return JsonResponse({
+                "success": False
+            })
+    else:
+            return HttpResponse("Method must be 'POST'")
+
+@csrf_exempt
+def like_post(request, id):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Food.objects.get(pk=id)
+            print(post)
+            try:
+                post.likers.add(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt
+def unlike_post(request, id):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Food.objects.get(pk=id)
+            print(post)
+            try:
+                post.likers.remove(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt
+def save_post(request, id):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Food.objects.get(pk=id)
+            print(post)
+            try:
+                post.savers.add(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt
+def unsave_post(request, id):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Food.objects.get(pk=id)
+            print(post)
+            try:
+                post.savers.remove(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt
+def follow(request, username):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            user = User.objects.get(username=username)
+            print(f".....................User: {user}......................")
+            print(f".....................Follower: {request.user}......................")
+            try:
+                (follower, create) = Order.objects.get_or_create(user=user)
+                follower.followers.add(request.user)
+                follower.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt  
+def unfollow(request, username):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            user = User.objects.get(username=username)
+            print(f".....................User: {user}......................")
+            print(f".....................Unfollower: {request.user}......................")
+            try:
+                follower = Order.objects.get(user=user)
+                follower.followers.remove(request.user)
+                follower.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+
+@csrf_exempt
+def comment(request, post_id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            data = json.loads(request.body)
+            comment = data.get('comment_text')
+            post = Food.objects.get(id=post_id)
+            try:
+                newcomment = Food.objects.create(post=post,commenter=request.user,comment_content=comment)
+                post.comment_count += 1
+                post.save()
+                print(newcomment.serialize())
+                return JsonResponse([newcomment.serialize()], safe=False, status=201)
+            except Exception as e:
+                return HttpResponse(e)
+    
+        post = Food.objects.get(id=post_id)
+        comments = Order.objects.filter(post=post)
+        comments = comments.order_by('-comment_time').all()
+        return JsonResponse([comment.serialize() for comment in comments], safe=False)
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+@csrf_exempt
+def delete_post(request, post_id):
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Food.objects.get(id=post_id)
+            if request.user == post.creater:
+                try:
+                    delet = post.delete()
+                    return HttpResponse(status=201)
+                except Exception as e:
+                    return HttpResponse(e)
+            else:
+                return HttpResponse(status=404)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return HttpResponseRedirect(reverse('login'))
+    
+print(delete_post)
+
